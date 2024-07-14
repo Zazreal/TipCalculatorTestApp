1e97e4f5ea7392366633adab43cc807b6013b59b
Zazreal <tomnowak1204@gmail.com>
2024-07-14 20:04:16 +0200

    Initial Commit Added a dynamic display that describes the tip. Added some colors in colors.xml Changed the background color

--- app/src/main/java/com/example/tipcalculatortestapp/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculatortestapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tipcalculatortestapp
 
+import android.animation.ArgbEvaluator
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,16 +9,8 @@ import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tipcalculatortestapp.ui.theme.TipCalculatorTestAppTheme
+import androidx.core.content.ContextCompat
 
 private const val TAG = "MainActivity"
 class MainActivity : ComponentActivity() {
@@ -26,7 +19,7 @@ class MainActivity : ComponentActivity() {
     private lateinit var tvTipPercent: TextView
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
-
+    private lateinit var tvTipOpinion: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,15 +30,18 @@ class MainActivity : ComponentActivity() {
         tvTipPercent = findViewById(R.id.tvTipPercent)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipOpinion = findViewById(R.id.tvTipOpinion)
 
         SeekBarTip.progress = 0
         tvTipPercent.text = "0%"
+        UpdateTipOpinion(0)
 
         SeekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 Log.i(TAG, "onProgressChanged $p1")
                 tvTipPercent.text = "$p1%"
                 CalculateTipAndTotal()
+                UpdateTipOpinion(p1)
             }
             override fun onStartTrackingTouch(p0: SeekBar?) {}
             override fun onStopTrackingTouch(p0: SeekBar?) {}
@@ -65,7 +61,7 @@ class MainActivity : ComponentActivity() {
     private fun CalculateTipAndTotal()
     {
         //Gets values from view
-        var BaseAmount: Double = 0.00
+        var BaseAmount: Double
         if(etBaseInput.text.isEmpty()){
             tvTipAmount.text = ""
             tvTotalAmount.text = ""
@@ -82,5 +78,28 @@ class MainActivity : ComponentActivity() {
         //updates view
         tvTipAmount.text = "%.2f".format(TipVal)
         tvTotalAmount.text = "%.2f".format(TotalVal)
+    }
+
+    private fun UpdateTipOpinion(TipPercent: Int)
+    {
+        //Selects text
+        val TipOpinion: String = when(TipPercent) {
+            in 0..9->"poor"
+            in 10..19 -> "ok"
+            in 20..29 -> "great"
+            in 30..30 -> "Amazing"
+            else -> "Error"
+        }
+
+        //Calculates the color
+        val color = ArgbEvaluator().evaluate(
+            TipPercent.toFloat()/SeekBarTip.max,
+            ContextCompat.getColor(this,R.color.Wort_Tip),
+            ContextCompat.getColor(this,R.color.Best_Tip)
+        ) as Int
+
+        //Sets text and color
+        tvTipOpinion.text = TipOpinion
+        tvTipOpinion.setTextColor(color)
     }
 }
